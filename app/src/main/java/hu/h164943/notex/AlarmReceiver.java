7dbc400a62077d456f47938a629c8bb7d25b4db3
tomitheninja <sudi.tamas@simonyi.bme.hu>
2023-05-01 05:02:40 +0200

    Initial commit

---
+++ app/src/main/java/hu/h164943/notex/AlarmReceiver.java
@@ -0,0 +1,13 @@
+package hu.h164943.notex;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        new NotificationHelper(context).send("It's time to shop something!");
+    }
+}
